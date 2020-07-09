b65bb3c55a51626f8bd2f6ba23044df39f65ed25
midomido_213 <midopoppo213@gmail.com>
2020-07-09 09:36:38 +0900

    ノートＰＣから変更

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/chatbot/config/config.php');
 
 // ログイン状態チェック
 if (!isset($_SESSION["userId"])) {
-    header("Location: https://takagi-lab.tk/chatbot/page/Logout.php");
+    header("Location: https://tkg-lab.tk/chatbot/page/Logout.php");
     exit;
 }
 
@@ -18,14 +19,31 @@ try{
   $admin = $stmt->fetchColumn();
 
   if($admin != 1){
-    header("Location: https://takagi-lab.tk//chatbot/page/Login.php");
+    header("Location: https://tkg-lab.tk/chatbot/page/lesson/2019c/index.php");
     exit;
   }
 }catch(PDOException $e){
 
 }
 
-$errorMessage = '';
+
+//DB登録処理
+$comment = $_POST['comment'];
+$student = $_POST['student'];
+$ta = $_POST['ta'];
+$chatId = $_POST['chatId'];
+try{
+  $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+  $stmt = $pdo->prepare('INSERT INTO taComment (student, ta, comment) VALUES (?, ?, ?)');
+  $stmt->execute(array($student, $ta, $comment));
+  $stmt = $pdo->prepare('UPDATE chatLog SET support = 1 where id = ?');
+  $stmt->execute(array($chatId));
+
+  $alert = '<div class="uk-alert-success" uk-alert><a class="uk-alert-close" uk-close></a><p>TA/SA 対応状況の登録が完了しました。</p><p>対応済みに変更しました。</p></div>';
+}catch(PDOException $e){
+
+}
 
 ?>
 
@@ -33,105 +51,183 @@ $errorMessage = '';
 <html>
   <head>
     <meta charset="utf-8">
-    <title>情報基礎数学botのページ</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>情報基礎数学botのページ</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <!-- UIkit CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
-    <link rel="stylesheet" href="https://takagi-lab.tk/chatbot/page/css/style.css" />
+      <link rel="stylesheet" href="https://tkg-lab.tk/chatbot/page/css/style.css" />
+      <link rel="stylesheet" href="https://tkg-lab.tk/chatbot/page/css/table.css" />
 
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+      <!-- BULMA CDN -->
+      <script src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" defer ></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
+      <!-- BULMA JS -->
+      <script src="https://cdn.jsdelivr.net/npm/vue-bulma-accordion@0.4.8/dist/vue-bulma-accordion.umd.min.js"></script>
 
   </head>
-  <body class="background">
-    <section class="uk-section uk-dark uk-background-cover">
-
-      <div class="title">
-        <h3>情報基礎数学 チャットボットによる振り返りシステム</h3>
-        <h3>２０１９年度 情報基礎数学Ｂ</h3>
-        <p><?php echo($_SESSION["userId"]); ?> でログイン中</p>
+  <body>
+    <!-- header -->
+    <header class="navbar">
+      <div class="navbar-brand">
+          <span class="navbar-item">
+              <span class="fas fa-robot"></span>チャットボットを利用した振り返り
+          </span>
       </div>
-
-      <!-- お知らせ -->
-      <section class="uk-section uk-section-xsmall">
-        <div class="uk-container">
-          <div class="uk-card uk-card-default uk-card-body">
-            <h3>お知らせ</h3>
-            <p>システムにアクセスありがとうございます．</p>
-            <p>TA・SAはチャットログを確認し，必要に応じて個別対応をお願いします．</p>
-            <p>何か分からないことがあれば，小菅まで問い合わせください．</p>
-            <p>g231r010@s.iwate-pu.ac.jp</p>
-            <!-- ブラウザ判定 -->
-            <?php
-            // 判定するのに小文字にする
-            $browser = strtolower($_SERVER['HTTP_USER_AGENT']);
-
-            // ユーザーエージェントの情報を基に判定
-            if (strstr($browser , 'edge')) {
-                // echo('ご使用のブラウザはEdgeです。');
-            } elseif (strstr($browser , 'trident') || strstr($browser , 'msie')) {
-                echo('<p>※注意※ご使用のブラウザはInternet Explorerです。</p>');
-                echo('<p>InternetExplorerではシステムが動作しない可能性があります．</p>');
-                echo('<p>推奨ブラウザは <a href="https://www.google.com/intl/ja_ALL/chrome/" target="_blank">GoogleChrome</a> です．</p>');
-            } elseif (strstr($browser , 'chrome')) {
-                // echo('ご使用のブラウザはGoogle Chromeです。');
-            } elseif (strstr($browser , 'firefox')) {
-                // echo('ご使用のブラウザはFirefoxです。');
-            } elseif (strstr($browser , 'safari')) {
-                // echo('ご使用のブラウザはSafariです。');
-            } elseif (strstr($browser , 'opera')) {
-                // echo('ご使用のブラウザはOperaです。');
-            } else {
-                echo('不明。');
-            }
-            ?>
-          </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <p><?php echo($_SESSION["userId"]); ?> でログイン中</p>
         </div>
-      </section>
-
-      <!-- メイン表示部分 -->
-      <?php
-      $name = $_SESSION['userId'];
-      $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
-      try{
-        $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-        $stmt = $pdo->prepare('SELECT admin FROM userData WHERE name = ?');
-        $stmt->execute(array($name));
-        $admin = $stmt->fetchColumn();
-        if($admin == 1){
-          echo '<!-- 管理者表示 --><section class="uk-section uk-section-xsmall"><div class="uk-container"><div class="uk-card uk-card-secondary uk-card-body"><h3>管理者限定表示</h3><ul class="uk-list">';
-          echo '<li><button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom" onclick="location.href=\'https://takagi-lab.tk/chatbot/admin/log/\'">チャットログ閲覧ページ</button></li>';
-          // echo '<li><button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom" onclick="location.href=\'bot_page/bot_dev.php\'">チャットボット開発動作確認ページ</button></li>';
-          // echo '<li><button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom" onclick="location.href=\'#\'">ユーザー管理（準備中）</button></li>';
-          echo '</ul></div></div></section>';
-        }
-      }catch(PDOException $e){
-        $errorMessage = 'エラーです';
-      }
-
-      ?>
-
-      <!-- 操作 -->
-      <section class="uk-section uk-section-xsmall">
-        <div class="uk-container">
-          <div class="uk-card uk-card-default uk-card-body">
-            <h3>その他　リンク</h3>
-            <div>
-              <ul class="uk-list">
-                <li><button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom" onclick="window.open('https://takagi-lab.tk/g031o008/plan/view/main/index.php', '_blank')">学習計画の作成</button></li>
-                <li><button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom" onclick="window.open('https://www.ipusoft-el.jp/mdl/', '_blank')">岩手県立大学Moodleへのリンク</button></li>
-                <li><button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom" onclick="window.open('https://solomon.uela.cloud/', '_blank')">共通基盤教育システムへのリンク</button></li>
-                <li><button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom" onclick="location.href='../../index.php'">トップページに戻る</button></li>
-                <li><button class="uk-button uk-button-danger uk-width-1-1 uk-margin-small-bottom" onclick="location.href='http://153.126.193.128/chatbot/page/Logout.php'">ログアウト</button></li>
-              </ul>
-            </div>
-          </div>
+        <div class="navbar-item">
+          <a href="https://tkg-lab.tk/chatbot/page/Logout.php"><i class="fa fa-user"></i>ログアウト</a>
         </div>
-      </section>
+      </div>
+    </header>
 
-    </section>
+    <!-- ライン -->
+    <div class="hero is-dark is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">情報基礎数学 計画/振り返りシステム 管理画面</h1>
+          <h2 class="subtitle">＠チャットログ閲覧ページ</h2>
+        </div>
+      </div>
+    </div>
+
+    <!-- メイン表示部分 -->
+    <main class="columns">
+      <div class="submenu column is-3">
+        <!-- side bar -->
+        <aside class="box">
+          <p>各学生のチャットログを閲覧できるページです。</p>
+          <p>チャットログを参考にし、個別指導を行ってください。</p>
+        </aside>
+        <aside class="box menu">
+           <p class="menu-label">
+              各種リンク
+           </p>
+           <ul class="menu-list">
+               <li><a href="https://tkg-lab.tk/g031o008/plan/view/main/index.php">学習計画の作成</a></li>
+               <li><a href="https://www.ipusoft-el.jp/mdl/">岩手県立大学Moodle</a></li>
+               <li><a href="https://solomon.uela.cloud/">共通基盤教育システム</a></li>
+            </ul>
+         </aside>
+ 　　　</div>
+      <div class="column">
+        <article class="box media">
+           <div class="media-content">
+              <div class="content">
+                 <p>理解度について</p>
+                 <p>理解度に応じてチャットログの背景色を変えています。</p>
+                 <span class="tag is-large" style="background:#ee5253">理解度0：全く理解ができていない</span>
+                 <span class="tag is-large" style="background:#ff6b6b">理解度1：まだ分からない箇所が多く不安である</span>
+                 <span class="tag is-large" style="background:#feca57">理解度2：少し理解しているが、曖昧な箇所もありどちらともいえない</span>
+                 <span class="tag is-large" style="background:#48dbfb">理解度3：概ね理解しており、不安が少ない</span>
+                 <span class="tag is-large" style="background:#1dd1a1">理解度4：今回の範囲に関しては完璧に理解している</span>
+              </div>
+           </div>
+        </article>
+        <article class="box media">
+           <form action="index.php" method="post">
+             <div class="field">
+               <label class="label">授業回</label>
+               <div class="select">
+                 <select name="lesson">
+                   <option>1</option>
+                   <option>2</option>
+                   <option>3</option>
+                   <option>4</option>
+                   <option>5</option>
+                   <option>6</option>
+                   <option>7</option>
+                 </select>
+               </div>
+             </div>
+             <div class="field">
+               <label class="label">グループ</label>
+               <div class="select">
+                 <select name="group">
+                   <option>1</option>
+                   <option>2</option>
+                   <option>3</option>
+                   <option>4</option>
+                   <option>5</option>
+                   <option>6</option>
+                   <option>7</option>
+                   <option>8</option>
+                   <option>9</option>
+                   <option>10</option>
+                 </select>
+               </div>
+              </div>
+              <button class="button is-primary">Submit</button>
+           </form>
+        </article>
+        <article class="box media">
+           <div class="media-content">
+              <div class="content">
+                <p><strong>確認画面</strong></p>
+
+                <!-- 表示部分 -->
+                <div id="app">
+                  <bulma-accordion>
+                <?php
+                  $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
+                  $userId = $_SESSION['userId'];
+                  $groupId = $_POST['group'];
+                  $lesson = $_POST['lesson'];
+                  try{
+                    $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+                    $stmt = $pdo->prepare('SELECT chatLog2020c.id AS id, chatLog2020c.name AS name, chatLog2020c.lesson AS lesson, chatLog2020c.level AS level, chatLog2020c.logAll AS logAll, chatLog2020c.timestamp AS timestamp, groupData2020c.userName AS name, groupData2020c.groupId AS groupId FROM chatLog2020c INNER JOIN groupData2020c ON groupData2020c.userName = chatLog2020c.name  WHERE groupId = ? AND lesson = ? ORDER BY level > 0 DESC, level ASC;');
+                    $stmt->execute(array($groupId, $lesson));
+
+                    while ($row = $stmt->fetch()) {
+                ?>
+                      <bulma-accordion-item class="level<?php echo nl2br($row['level']); ?>">
+                        <div slot="title">
+                          <p>第<?php echo nl2br($row['lesson']); ?>回 振り返り履歴　ユーザー：<?php echo nl2br($row['name']); ?></p>
+                          <p>理解度:<?php echo nl2br($row['level']); ?></p>
+                          <p>回答時間：<?php echo $row['timestamp']; ?></p>
+                        </div>
+                        <div slot="content">
+                         <?php echo nl2br($row['logAll']); ?>
+                       </div>
+                      </bulma-accordion-item>
+
+                <?php
+                      }
+                    }catch(PDOException $e){
+                      $errorMessage = 'エラーです';
+                    }
+                ?>
+                  </bulma-accordion>
+                </div>
+
+              </div>
+
+           </div>
+        </article>
+        <article class="box media">
+           <div class="media-content">
+              <div class="content">
+                 <p><strong>前のページに戻る</strong></p>
+                 <button class="button is-primary" onclick="location.href='https://tkg-lab.tk/chatbot/admin/'">こちらをクリック</button>
+              </div>
+           </div>
+        </article>
+　　　 </div>
+　　 </main>
+
+    <!-- footer -->
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+           <p>takagi-lab</p>
+        </div>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/vue/latest/vue.min.js"></script>
+    <script src="https://tkg-lab.tk/chatbot/page/lesson/2020c/js/bulma-accordion.js"></script>
 
   </body>
-</html>
